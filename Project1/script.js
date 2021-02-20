cc0d4f346c2e19a4833766b123b5c3669f700c42
NBCharro <blancocharron@gmail.com>
2021-02-20 19:34:55 +0100

    changed the vision of project1

--- Project1/script.js
+++ Project1/script.js
@@ -1,208 +1,27 @@
 'use strict';
-const ver = function () {
-    modal.style.display = 'block';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.nometal');
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
-// Get the modal
-var modal = document.querySelector('.modal');
-const item = document.getElementsByClassName('nometal');
-for (var i = 0; i < item.length; i++) {
-    item[i].addEventListener('click', ver);
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
-document.querySelector('.elementos').addEventListener('click', function (e) {
-    if (e.target.nodeName == 'LI') {
-        //console.log(
-        //    e.target.getElementsByTagName('h2')[0].innerText + ' clickado'
-        //)
-        console.log(e.target.innerText + ' clickado');
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
     }
 });
-const texto = function (Element) {
-    return Element.target.innerText;
-};
-document.querySelector('.elementos').addEventListener('click', texto);
 
-const lista = [
-    'H',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'He',
-    'Li',
-    'Be',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'B',
-    'C',
-    'N',
-    'O',
-    'F',
-    'Ne',
-    'Na',
-    'Mg',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    'Al',
-    'Si',
-    'P',
-    'S',
-    'Cl',
-    'Ar',
-    'K',
-    'Ca',
-    'Sc',
-    'Ti',
-    'V',
-    'Cr',
-    'Mn',
-    'Fe',
-    'Co',
-    'Ni',
-    'Cu',
-    'Zn',
-    'Ga',
-    'Ge',
-    'As',
-    'Se',
-    'Br',
-    'Kr',
-    'Rb',
-    'Sr',
-    'Y',
-    'Zr',
-    'Nb',
-    'Mo',
-    'Tc',
-    'Ru',
-    'Rh',
-    'Pd',
-    'Ag',
-    'Cd',
-    'In',
-    'Sn',
-    'Sb',
-    'Te',
-    'I',
-    'Xe',
-    'Cs',
-    'Ba',
-    'La',
-    'Ce',
-    'Pr',
-    'Nd',
-    'Pm',
-    'Sm',
-    'Eu',
-    'Gd',
-    'Tb',
-    'Dy',
-    'Ho',
-    'Er',
-    'Tm',
-    'Yb',
-    'Lu',
-    'Hf',
-    'Ta',
-    'W',
-    'Re',
-    'Os',
-    'Ir',
-    'Pt',
-    'Au',
-    'Hg',
-    'Tl',
-    'Pb',
-    'Bi',
-    'Po',
-    'At',
-    'Rn',
-    'Fr',
-    'Ra',
-    'Ac',
-    'Th',
-    'Pa',
-    'U',
-    'Np',
-    'Pu',
-    'Am',
-    'Cm',
-    'Bk',
-    'Cf',
-    'Es',
-    'Fm',
-    'Md',
-    'No',
-    'Lr',
-    'Rf',
-    'Db',
-    'Sg',
-    'Bh',
-    'Hs',
-    'Mt',
-];
-let str = [];
-const repetido = function () {
-    for (let i = 0; i < lista.length; i++) {
-        str[i] = `<li class="nometal">${lista[i]}</li>`;
-        if (lista[i] == '') {
-            str[i] = `<li class="vacio">${lista[i]}</li>`;
-        }
-        if (
-            lista[i] == 'B' ||
-            lista[i] == 'Si' ||
-            lista[i] == 'Ge' ||
-            lista[i] == 'As' ||
-            lista[i] == 'Sb' ||
-            lista[i] == 'Te' ||
-            lista[i] == 'Po'
-        ) {
-            str[i] = `<li class="nometal">${lista[i]}</li>`;
-        }
-        if (lista[i] == '') {
-            str[i] = `<li class="vacio">${lista[i]}</li>`;
-        }
-        if (lista[i] == '') {
-            str[i] = `<li class="vacio">${lista[i]}</li>`;
-        }
-    }
-    console.log(str[30]);
-    document.getElementById('lista').innerHTML = str;
-};
-
-document.querySelector('.elementos').addEventListener('click', repetido);
-document.querySelector('.elementos').addEventListener('onload', repetido);
+document.write('Prueba');
